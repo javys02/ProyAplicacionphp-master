8134206f36e5620ec170ebef9f169ab8fbf085f2
Javier Balboa <jbalboa@CI-LAPAZ.GOV.BO>
2017-09-17 00:13:41 -0400

    2da actualizacion

--- AppScript/Acreditar.php
+++ AppScript/Acreditar.php
@@ -2,9 +2,9 @@
 
 require_once("../Settings/conexion.php");
 
-$Email = $_POST['Email'];
+$Email = $_POST['Carnet'];
 
-$Consulta = "SELECT idUsuario, Nombre FROM Usuario WHERE Email = '$Email'";
+$Consulta = "SELECT idUsuario, Nombre, Apellido, Carnet, Email FROM Usuario WHERE Carnet = '$Carnet'";
 $res_Consulta = mysqli_query( $conexion, $Consulta ) or die ( "Error en la consulta del listado en la base de datos");
 
 $row_Consulta = mysqli_fetch_assoc($res_Consulta);
@@ -12,10 +12,11 @@ $row_Consulta = mysqli_fetch_assoc($res_Consulta);
 if(!is_null($row_Consulta['idUsuario']) )
 {  // verifica si el dni ingresado existe en la base de datos
 	
-	$data = array('Rpta' => 1 ,'Nombre' => $row_Consulta['Nombre']); 
+	$data = array('Rpta' => 1 ,'Nombre' => $row_Consulta['Nombre'],'Apellido' => $row_Consulta['Apellido'],'Carnet' => $row_Consulta['Carnet'],'Email' => $row_Consulta['Email']); 
+
 	print (json_encode($data)); 
 	
-	$Acreditado = "UPDATE Usuario SET Estado = 1 WHERE Email = '$Email'";
+	$Acreditado = "UPDATE Usuario SET Estado = 1 WHERE Carnet = '$Carnet'";
 	mysqli_query( $conexion, $Acreditado);
 }
 else
--- RegistrarUsuarios.php
+++ RegistrarUsuarios.php
@@ -1,22 +1,24 @@
 <?php 
-include_once('Settings/conexion.php');
-include_once('static/lib/phpqrcode/qrlib.php');
+	include_once('Settings/conexion.php');
+	include_once('static/lib/phpqrcode/qrlib.php');
 
-$Nombre = $_POST['name']; 
-$Apellido = $_POST['apellido'];
-$Carnet = $_POST['carnet'];
-$Email = $_POST['email']; 
+	$Nombre = $_POST['name']; 
+	$Apellido = $_POST['apellido'];
+	$Carnet = $_POST['carnet'];
+	$Email = $_POST['email']; 
 
-$filename = "QRUsuarios/".$Carnet.".png";
-$tamCodeQR = 10;
-$CorrectionError = "M"; // M Q (H mejor)
-QRcode::png($Carnet, $filename, $CorrectionError, $tamCodeQR, 2); 
+	$filename = "QRUsuarios/".$Carnet.".png";
+	$tamCodeQR = 10;
+	$CorrectionError = "M"; // M Q (H mejor)
 
-$UsuarioNuevo = "INSERT INTO Usuario (Nombre, Apellido, Carnet, Email ) value ('$Nombre', '$Apellido', '$Carnet', '$Email')";
-//mysql_query($UsuarioNuevo);
+	if (!is_null($Carnet) && !validaRequerido($Nombre) && !validaRequerido($Apellido)) {
+	  	QRcode::png($Carnet, $filename, $CorrectionError, $tamCodeQR, 2); 
 
-$resultado = mysqli_query( $conexion, $UsuarioNuevo ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+		$UsuarioNuevo = "INSERT INTO Usuario (Nombre, Apellido, Carnet, Email ) value ('$Nombre', '$Apellido', '$Carnet', '$Email')";
 
-header("Location:UsuarioRegistrado.php?carnet=".$Carnet."&nombre=".$Nombre."&apellido=".$Apellido."&email=".$Email);
+		$resultado = mysqli_query( $conexion, $UsuarioNuevo ) or die ( "Algo ha ido mal en la consulta a la base de datos");
 
+		header("Location:UsuarioRegistrado.php?carnet=".$Carnet."&nombre=".$Nombre."&apellido=".$Apellido."&email=".$Email);
+
+	} 
 ?>
